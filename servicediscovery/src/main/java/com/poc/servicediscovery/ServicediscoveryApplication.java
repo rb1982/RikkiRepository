fe3b5d2c40da9b8a5d5fdb489c176ad07b5331f7
rikki.bindra <bindra.rikki@gmail.com>
2019-01-10 16:47:25 +0530

    changes to run multiple eureka instances

--- servicediscovery/src/main/java/com/poc/servicediscovery/ServicediscoveryApplication.java
+++ servicediscovery/src/main/java/com/poc/servicediscovery/ServicediscoveryApplication.java
@@ -1,7 +1,8 @@
 package com.poc.servicediscovery;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
@@ -9,7 +10,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class ServicediscoveryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServicediscoveryApplication.class, args);
+		new SpringApplicationBuilder(ServicediscoveryApplication.class).web(WebApplicationType.SERVLET).run(args);
+		//SpringApplication.run(ServicediscoveryApplication.class, args);
+		
 	}
 
 }
