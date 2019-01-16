48e89304f832d57a92214f29fbcf094e8ed28395
rikki.bindra <bindra.rikki@gmail.com>
2019-01-16 19:38:55 +0530

    integrated spring bus so that configuration properties are refreshed in all services at one go

--- microservices2/src/main/java/com/poc/microservices2/MyRestController.java
+++ microservices2/src/main/java/com/poc/microservices2/MyRestController.java
@@ -3,16 +3,20 @@ package com.poc.microservices2;
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class MyRestController {
 	
 	@Autowired
 	private Environment env;
-	
+	@Value ("${test.value}")
+	private String value;
 	
 	@RequestMapping("/random")
 	public String random() {
@@ -24,7 +28,7 @@ public class MyRestController {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		return "Random number generated is " + Math.random() + " on PORT " + env.getProperty("local.server.port");
+		return "Test value is " + value + " & Random number generated is " + Math.random() + " on PORT " + env.getProperty("local.server.port");
 	}
 
 }
